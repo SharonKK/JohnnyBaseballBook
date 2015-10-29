61ae4c5a0b8b1e288738f6257a631ea08ecf53d3
Sharon Kearney <Sharonkayekearney1@gmail.com>
2015-10-29 14:21:56 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* when an image is clicked, the next page will display. */
+var HTMLImageElement = {};
+var HTMLDivElement = {};
+
+function changepage() {HTMLImageElement.onclick = document.getElementById(); }
+
+changepage();
